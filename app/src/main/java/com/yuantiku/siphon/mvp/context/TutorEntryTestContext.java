ce52808b4fd81893f83fbe95b6b0e4b7a8b20dab
wanghb <wanghb@fenbi.com>
2015-12-20 16:13:48 +0800

    add entry for test serial

--- app/src/main/java/com/yuantiku/siphon/mvp/context/TutorEntryTestContext.java
+++ app/src/main/java/com/yuantiku/siphon/mvp/context/TutorEntryTestContext.java
@@ -11,11 +11,14 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.yuantiku.siphon.R;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+import com.yuantiku.siphon.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by wanghb on 15/10/21.
@@ -29,6 +32,12 @@ public class TutorEntryTestContext extends BaseContext {
 
     @Bind(R.id.season_id)
     TextView seasonIdTextView;
+
+    @Bind(R.id.teacher_id_for_serial)
+    TextView teacherIdForSerialTextView;
+
+    @Bind(R.id.serial_id)
+    TextView serialIdTextView;
 
     @Nullable
     @Override
@@ -59,11 +68,36 @@ public class TutorEntryTestContext extends BaseContext {
         launch(type, id);
     }
 
+    @OnClick(R.id.launch_serial)
+    public void launchSerial() {
+        String type = "Serial";
+        Map<String, String> map = new HashMap<>();
+        map.put("id", serialIdTextView.getText().toString());
+        map.put("teacherId", teacherIdForSerialTextView.getText().toString());
+        launch(type, map);
+    }
+
     private void launch(String type, String id) {
+        Map<String, String> map = new HashMap<>();
+        map.put("id", id);
+        launch(type, map);
+    }
+
+    private void launch(String type, Map<String, String> query) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String key : query.keySet()) {
+            stringBuilder.append(key)
+                    .append("=")
+                    .append(query.get(key))
+                    .append("&");
+        }
+        if (stringBuilder.length() > 0) {
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        }
         try {
             Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse(String.format("tutor1f6f42334e1709a4://openRecommended%s?id=%s",
-                    type, id)));
+            intent.setData(Uri.parse(String.format("tutor1f6f42334e1709a4://openRecommended%s?%s",
+                    type, stringBuilder.toString())));
             startActivity(intent);
         } catch (ActivityNotFoundException e) {
             Toast.makeText(getActivity(), "手机上没有安装猿辅导或者猿辅导没有提供支持", Toast.LENGTH_LONG).show();
